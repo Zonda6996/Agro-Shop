6004299a1449eb27d069c0e4772b6044c9b37f62
Zonda6996 <ifresh3d@gmail.com>
2026-01-28 15:42:48 +0500

    Add new fonts

---
+++ app/shared/ui/fonts/fonts.ts
@@ -0,0 +1,13 @@
+import { Poppins, Inter } from 'next/font/google'
+
+export const poppins = Poppins({
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
+	variable: '--font-poppins',
+})
+
+export const inter = Inter({
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
+	variable: '--font-inter',
+})
