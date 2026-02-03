321c0252b1d7f0e1391d586ec1dcb2bb3db0c5b5
Zonda6996 <ifresh3d@gmail.com>
2026-02-03 16:49:33 +0500

    Header update, add home page template

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,21 @@
+import { Container } from "@/widgets/container/container";
+
 export default async function Home() {
-	return <div className="">Public Home Page</div>
+	return (
+		<div className=''>
+			<Container className="">
+				<h1 className='text-4xl heading '>Домашнаяя страница</h1>
+				<div>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, a
+						nemo! Adipisci voluptas fuga aspernatur dolorum, voluptatibus ipsum
+						neque? Maiores assumenda esse porro, alias et vero unde illum cum?
+						Labore dolores numquam saepe deserunt laboriosam dignissimos, quos,
+						rem doloribus distinctio iure magni blanditiis, maiores suscipit.
+						Magnam incidunt nihil voluptatem quia.
+					</p>
+				</div>
+			</Container>
+		</div>
+	)
 }
---
+++ app/widgets/header/header.tsx
@@ -0,0 +1,48 @@
+import { ROUTES } from '@/shared/lib/routes'
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '../../../public/logo/logo.svg'
+import { Separator } from '@/shared/ui/separator'
+import { Navlink } from '@/shared/ui/navlink'
+
+export const Header = () => {
+	return (
+		<header className='w-full bg-white/90 shadow-sm'>
+			<div className='container mx-auto px-6 py-4 flex justify-between items-center'>
+				<Link href={ROUTES.HOME}>
+					<Image
+						src={Logo}
+						alt='Logo'
+						width={120}
+						height={40}
+						loading='eager'
+					/>
+				</Link>
+				<nav>
+					<ul className='flex items-center gap-3 p-1 rounded-full bg-green-100/50 shadow-sm'>
+						<Navlink href={ROUTES.HOME}>Главная</Navlink>
+						<Navlink href={ROUTES.PRODUCTS}>Ассортимент</Navlink>
+						<Navlink href='/catalog'>Каталог</Navlink>
+						<Navlink href='/partners'>Партнёры</Navlink>
+						<Navlink href='/about'>О нас</Navlink>
+					</ul>
+				</nav>
+				<div className=' flex gap-3 items-center font-medium rounded-full '>
+					<Link
+						className='py-1.5 px-2 rounded-full hover:bg-primary-hover transition'
+						href={'/'}
+					>
+						Войти
+					</Link>
+					<Separator className='w-[0.8px]! h-4' orientation='vertical' />
+					<Link
+						className='py-1.5 px-2 rounded-full hover:bg-primary-hover transition'
+						href={'/'}
+					>
+						Регистрация
+					</Link>
+				</div>
+			</div>
+		</header>
+	)
+}
