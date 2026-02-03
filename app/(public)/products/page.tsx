6f3100d7cb4f1d6eb4ac7f943a6d9c76b9cef0f6
Zonda6996 <ifresh3d@gmail.com>
2026-02-03 16:49:59 +0500

    Products page

---
+++ app/(public)/products/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/widgets/container/container"
+
+const ProductsPage = () => {
+	return <div>
+		<Container>
+			<h1>Products Page</h1>
+		</Container>
+	</div>
+}
+
+export default ProductsPage
