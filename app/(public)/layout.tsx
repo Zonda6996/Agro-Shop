2da91ddaf562e1bb2d17a9c84950e6472c9b6fd4
Zonda6996 <ifresh3d@gmail.com>
2026-02-03 16:47:50 +0500

    Add Header

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,10 +1,12 @@
+import { Header } from '@/widgets/header/header'
+
 export default function AppLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<>
-			{/* header */}
-			<div>{children}</div>
+			<Header />
+			<main className='min-h-screen mt-5 bg-background'>{children}</main>
 		</>
 	)
 }
