6610de5666ea7e0cc605434beca8a33a320a043d
Zonda6996 <ifresh3d@gmail.com>
2026-03-10 17:50:46 +0500

    refactor: update routes

--- app/shared/lib/routes.ts
+++ app/shared/lib/routes.ts
@@ -1,11 +1,13 @@
 export const ROUTES = {
 	HOME: '/',
 	PRODUCTS: '/products',
-	PRODUCT: (id: number) => `products/${id}`,
+	PRODUCT: (id: number) => `/products/${id}`,
 	CART: '/cart',
 	CHECKOUT: '/checkout',
 	LOGIN: '/login',
 	REGISTER: '/register',
 	ACCOUNT: '/account',
 	ORDER: (id: number) => `/orders/${id}`,
+	ACCOUNT_ORDERS: '/account/orders',
+	ACCOUNT_PROFILE: '/account/profile',
 }
