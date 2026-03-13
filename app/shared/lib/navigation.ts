cbd11d15ff51493e42fdceff390df5cca429dfd6
Zonda6996 <ifresh3d@gmail.com>
2026-03-13 21:56:06 +0500

    feat: add navigation utility library

---
+++ app/shared/lib/navigation.ts
@@ -0,0 +1,9 @@
+import { ROUTES } from './routes'
+
+export const navLinks = [
+	{ href: ROUTES.HOME, label: 'Главная' },
+	{ href: ROUTES.PRODUCTS, label: 'Ассортимент' },
+	{ href: ROUTES.CART, label: 'Корзина' },
+	{ href: '/#partners', label: 'Партнёры' },
+	{ href: '/#about', label: 'О нас' },
+]
